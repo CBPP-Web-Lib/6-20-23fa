bc9028aa224482b5485e2ad46fa1608a3cebe17d
Nick Kasprak <nkasprak@gmail.com>
2023-06-21 11:04:38 -0400

    tweaks

--- node/src/app.js
+++ node/src/app.js
@@ -16,6 +16,10 @@ const script_id = "script_" + id;
 const script_sel = "#" + script_id;
 const script_url = document.getElementById(script_id).src.split("?")[0];
 const url_base = script_url.replace(/js\/app[\.min]*\.js/g,"");
+const shareable_fonts = document.createElement("link");
+shareable_fonts.setAttribute("rel","stylesheet");
+shareable_fonts.setAttribute("href", "https://use.typekit.net/nnt7xeb.css");
+document.querySelector("head").appendChild(shareable_fonts);
 
 const data = parse_data_string(require("raw-loader!./data.txt").default);
 
@@ -43,12 +47,12 @@ Promise.all([
 
   var dot_model = [];
   data.forEach((row)=>{
-    var r = Math.random()*0.1;
+    var r = Math.random()*0.5;
     var theta = Math.random()*2*Math.PI;
     var x = r*Math.cos(theta);
     var y = r*Math.sin(theta);
     y += 0.6;
-    x += (row[0] === 0 ? 0.2 : 0.8);
+    x += (row[0] === 0 ? 0.15 : 0.85);
     var p = {x, y};
     p.worked = row[0];
     p.working = row[0];
@@ -66,9 +70,9 @@ Promise.all([
 
   const container = $(sel);
   const viewWidth = 90;
-  const viewHeight = 100/1.91 - 10;
-  const viewMargin = 5;
-  const svg = d3.select(sel).append("svg")
+  const viewMargin = 2;
+  const viewHeight = 100/1.91 - viewMargin*2;
+  const svg_el = d3.select(sel).append("svg")
     .attr("xmlns", "http://www.w3.org/2000/svg")
     .attr("viewBox",[
       0 - viewMargin,
@@ -76,20 +80,22 @@ Promise.all([
       viewWidth + viewMargin*2,
       viewHeight + viewMargin*2
     ].join(" "));
-
+  const svg = svg_el.append("g")
+    .attr("class","main-g")
+    .style("opacity", 0);
   const labels = svg.append("g")
     .attr("class","labels");
 
   var html_container = labels.append("foreignObject")
     .attr("x", vx(0))
-    .attr("y", vy(0.35))
+    .attr("y", vy(0.33))
     .attr("width", viewWidth)
     .attr("height", 40);
   var s1 = html_container
     .append("xhtml:p")
     .attr("xmlns", "http://www.w3.org/1999/xhtml")
     .text("At any given time, about half of adults subject to the SNAP time limit are working.")
-    .style("opacity", 1);
+    .style("opacity", 0);
   var s2 = html_container.append("xhtml:p")
     .attr("xmlns", "http://www.w3.org/1999/xhtml")
     .text("But many SNAP participants move in and out of work frequently.")
@@ -100,20 +106,22 @@ Promise.all([
     .style("opacity", 0)
     .style("margin-top","1em");
 
-  labels.append("text")
+  var not_working_label = labels.append("text")
     .attr("class","fade-before-end")
     .text("Not working")
     .attr("x", vx(0.2))
     .attr("y", vy(0.5))
+    .attr("opacity", 0)
     .attr("text-anchor","middle")
     .attr("fill", "#fff")
     .attr("font-size", "4")
 
-  labels.append("text")
+  var working_label = labels.append("text")
     .attr("class","fade-before-end")
     .text("Working")
     .attr("x", vx(0.8))
     .attr("y", vy(0.5))
+    .attr("opacity", 0)
     .attr("text-anchor","middle")
     .attr("fill", "#fff")
     .attr("font-size", "4");
@@ -125,7 +133,8 @@ Promise.all([
     .attr("text-anchor","start")
     .attr("fill","#FFCE6D")
     .attr("font-size",5)
-    .attr("font-weight","bold");
+    .attr("font-weight","bold")
+    .attr("font-family","rift");
   
   labels.append("text")
     .text("But Instability Overstates Joblessness")
@@ -134,13 +143,14 @@ Promise.all([
     .attr("text-anchor","start")
     .attr("fill","#FFCE6D")
     .attr("font-size",4)
-    .attr("font-weight","bold");
+    .attr("font-weight","bold")
+    .attr("font-family","rift");
 
   labels.append("text")
     .attr("class","fade-until-end")
     .text("Never worked")
     .attr("x", vx(0.2))
-    .attr("y", vy(0.71))
+    .attr("y", vy(0.72))
     .attr("text-anchor","middle")
     .attr("fill", "#fff")
     .attr("font-size", "4")
@@ -149,14 +159,15 @@ Promise.all([
     .attr("class","fade-until-end")
     .text("Worked at some point")
     .attr("x", vx(0.8))
-    .attr("y", vy(0.75))
+    .attr("y", vy(0.76))
     .attr("text-anchor","middle")
     .attr("fill", "#fff")
     .attr("font-size", "4");
 
   var legend = svg.append("g")
     .attr("class","legend")
-    .attr("opacity", 0);
+    .attr("opacity", 0)
+    .attr("transform","translate(0,17)");
 
   legend.append("text")
     .attr("class","legend")
@@ -164,7 +175,7 @@ Promise.all([
     .attr("fill", "#fff")
     .attr("font-size", "2")
     .attr("x", vx(0))
-    .attr("y", 16);
+    .attr("y", 0);
 
   legend.append("text")
     .attr("class","legend")
@@ -172,19 +183,19 @@ Promise.all([
     .attr("fill", "#fff")
     .attr("font-size", "2")
     .attr("x", vx(0.3))
-    .attr("y", 16);
+    .attr("y", 0);
 
   legend.append("circle")
     .attr("fill", "rgba(255, 255, 255, 0.3)")
     .attr("r", 1)
     .attr("cx", 13)
-    .attr("cy", 15.3);
+    .attr("cy", -.7);
 
   legend.append("circle")
     .attr("fill", "rgba(200, 200, 200, 1)")
     .attr("r", 1)
     .attr("cx", 44.5)
-    .attr("cy", 15.3);
+    .attr("cy", -.7);
 
 
   svg.selectAll(".fade-until-end")
@@ -193,38 +204,46 @@ Promise.all([
   var dot_size = 0.012;
 
   const force_to_center = function(strength, center_assign) {
-    return function(alpha) {
-      dot_model.forEach((p)=>{
-        var center = center_assign(p);
-        var x_off = p.x - center[0];
-        var y_off = p.y - center[1];
-        var dist_squared = Math.pow(x_off, 2) + Math.pow(y_off, 2);
-        var k = alpha*strength;
-        p.vx = -x_off * k * 2*Math.min(dist_squared, 0.05);
-        p.vy = -y_off * k * 2*Math.min(dist_squared, 0.05);
-      })
+    var ForceToCenter = function() {
+      this.force = function(alpha) {
+        dot_model.forEach((p)=>{
+          var center = center_assign(p);
+          var x_off = p.x - center[0];
+          var y_off = p.y - center[1];
+          var dist_squared = Math.pow(x_off, 2) + Math.pow(y_off, 2);
+          var k = alpha*strength;
+          p.vx += (-x_off * k * dist_squared);
+          p.vy += (-y_off * k * dist_squared);
+        })
+      }
+      this.strength = function(_strength) {
+        strength = _strength;
+      }
     }
+    return new ForceToCenter();
   }
 
   const boundary_force = forceBoundary(-1, 0.53, 2, 0.7)
     .strength(0.05)
 
   const working_groups = function(p) {
-    var center = [0.2,0.6];
+    var center = [0.15,0.6];
     if (p.working) {
-      center = [0.8,0.6];
+      center = [0.85,0.6];
     }
     return center;
   }
-
+  setTimeout(function() {
+    svg.style("opacity", 1);
+  }, 100);
   const initial_simulation = forceSimulation(dot_model)
-    .force("force_center", force_to_center(0.1, working_groups))
-   // .force("boundary_force", boundary_force)
+    .force("force_center", force_to_center(2, working_groups).force)
+    //.force("boundary_force", boundary_force)
     .force("collide", d3.forceCollide(dot_size*1.4))
     .stop()
     .alphaDecay(0)
-    .tick(200);
-  
+    .tick(2000)
+    .stop();
 
   var dot_layer = svg.append("g")
     .attr("class","dots");
@@ -241,6 +260,7 @@ Promise.all([
         .attr("r", vx(dot_size)-vx(0))
         .attr("fill","#0c61a4")
         .attr("stroke-width", 0)
+        .attr("fill-opacity", 0)
         .attr("fill", d.worked ? "rgba(200, 200, 200, 1)" : "rgba(255, 255, 255, 0.3)")
     })
 
@@ -258,12 +278,34 @@ Promise.all([
     .on("tick", tick_update);
   
   return;*/
+
+
+  
+  dot_model.forEach((dot)=>{
+    dot.vx = 0;
+    dot.vy = 0 ;
+    console.log(dot);
+  });
+
   const main_simulation = forceSimulation(dot_model)
-    .force("force_center", force_to_center(10, working_groups))
+    .force("force_center", force_to_center(10, working_groups).force)
     .force("boundary_force", boundary_force)
     .force("collide", d3.forceCollide(dot_size*1.4))
-    .alpha(0.05)
     .alphaDecay(0);
+
+  var alpha = 0.0;
+  var alpha_increase = setInterval(function() {
+    alpha += 0.0001;
+    if (alpha > 0.01) {
+      clearInterval(alpha_increase);
+      s1.style("opacity", 1);
+      svg.selectAll("g.person circle").attr("fill-opacity", 1);
+      working_label.attr("opacity", 1);
+      not_working_label.attr("opacity", 1);
+    }
+    main_simulation.alpha(alpha);
+    main_simulation.restart();
+  }, 10)
 
   main_simulation.on("tick", tick_update);
 
@@ -286,6 +328,9 @@ Promise.all([
           .attr("opacity", 0);
         final_simulation.restart();
         s3.style("opacity", 1);
+        setTimeout(function() {
+          svg.style("opacity",0);
+        }, 5000);
       }, 2000);
     }
   }
@@ -295,18 +340,18 @@ Promise.all([
     month_timer = setInterval(move_people, 20);
     s2.style("opacity", 1);
     legend.attr("opacity", 1);
-  }, 2000);
+  }, 6000);
 
-  var final_center = force_to_center(10, function(p) {
-    var center = [0.2,0.55];
+  var final_center = force_to_center(5, function(p) {
+    var center = [0.1,0.55];
     if (p.worked) {
-      center = [0.8,0.6];
+      center = [0.87,0.6];
     }
     return center;
   })
 
   var final_simulation = forceSimulation(dot_model)
-    .force("to_group", final_center)
+    .force("to_group", final_center.force)
     .force("collide", d3.forceCollide(dot_size*1.3))
     .force("boundary_force", boundary_force)
     .alpha(0.03)
