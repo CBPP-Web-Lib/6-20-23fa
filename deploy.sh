7f1ced7fac56cdfb9074994080015e09e7f9ce90
Nicholas Kasprak <nkasprak@gmail.com>
2023-07-27 12:18:36 -0400

    fix source link

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 rsync -av \
   -e "/usr/bin/ssh" \
-  ./html/ cbppapps@apps.cbpp.org:/home/cbppapps/apps.cbpp.org/6-20-23fa_rev7-25-23/
+  ./html/ cbppapps@apps.cbpp.org:/home/cbppapps/apps.cbpp.org/6-20-23fa_rev7-27-23/
 
 rsync -av \
   -e "/usr/bin/ssh" \
-  ./node/prod/ cbppapps@apps.cbpp.org:/home/cbppapps/apps.cbpp.org/6-20-23fa_rev7-25-23/js/
+  ./node/prod/ cbppapps@apps.cbpp.org:/home/cbppapps/apps.cbpp.org/6-20-23fa_rev7-27-23/js/
